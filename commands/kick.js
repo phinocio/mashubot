a4d33e4ac5453d3b154ef8d0d2add1e73c5de35e
Phinocio <github@phinocio.com>
2021-08-20 14:19:12 -0600

    added kick command

---
+++ commands/kick.js
@@ -0,0 +1,34 @@
+module.exports = {
+	name: "kick",
+	title: "Kick User",
+	description: "Summons the almighty kickhammer!",
+	help: "kick [@username] [reason]",
+	admin: true,
+	async execute(message, args, Discord, fs, commandFiles, prefix, client) {
+	let member = undefined;
+	if (message.mentions.members && message.mentions.members.size != 0) {
+		member = message.mentions.members.first();
+	} else {
+		member = await message.guild.members.fetch(args[0]);
+	}
+	if (member.id == message.member.id) {
+		message.delete();
+		return message.reply("You can't ban yourself, silly!");
+	} else if (await !member.kickable) {
+		message.delete();
+		return message.reply(args[0] + " cannot be kicked!");
+	} else {
+		try {
+		await member.send(
+			"You have been kicked from RingComics' Discord Server for: " +
+			args.slice(1).join(" ")
+		);
+		} catch (e) {}
+		await member.kick();
+		message.reply(
+		"User ID " + member + " has been kicked for: " + args.slice(1).join(" ")
+		);
+		message.delete();
+	}
+	},
+};
